485c9577401b127b4af25930d32491605eb9782a
Alisa <alisa.blakeney@gmail.com>
2020-10-19 07:37:29 +0800

    add file structure for server

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const projectRouter = require("./routes/project_routes")
+
+const port = 6969
+
+const app = express()
+
+const db = "mongodb://localhost/project_tracker";
+mongoose.connect(
+    db, 
+    {
+        useNewUrlParser: true;
+        useUnifiedTopology: true; 
+        useFindAndModify: false;
+    },
+    err=>{
+        if (err){
+            console.log("You done mucked the db", err)
+        } else {
+            console.log("connecto to db")
+        }
+    }
+)
+
+
+app.get("/", (req, res)={
+    console.log("got rooted")
+    res.json("HOME");
+});
+
+app.use("/projects", projectRouter);
+
+app.listen(port, ()=>console.log(`yoooo, smoke ${port} every day`))
