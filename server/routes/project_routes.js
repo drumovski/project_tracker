229f9922f478e925f5f93a5dc32eb26ffc13f618
Alisa <alisa.blakeney@gmail.com>
2020-10-19 08:42:11 +0800

    outline crud functions

--- server/routes/project_routes.js
+++ server/routes/project_routes.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const getProjects = require("./")
+const {
+    getProjects,
+    getProject,
+    makeProject, 
+    removeProject, 
+    changeProject
+ } = require("../controllers/project_controller")
 
-router.get("/", getProjects)
+router.get("/", getProjects);
+
+router.get("/:id", getProject);
+
+router.post("/", makeProject);
+
+router.delete("/:id", removeProject);
+
+router.put("/:id", changeProject);
+
 
 module.exports = router;
