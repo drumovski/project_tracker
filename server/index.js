40b46711273190a262bb09f6ac197dfe2bb7c41d
drumovski <jason.stacy@gmail.com>
2020-10-19 14:29:19 +1100

    merged with front

--- server/index.js
+++ server/index.js
@@ -1,34 +1,38 @@
-// const express = require("express");
-// const mongoose = require("mongoose")
-// const projectRouter = require("./routes/project_routes")
+const express = require("express");
+const mongoose = require("mongoose")
+const projectRouter = require("./routes/project_routes")
+const handlebars = require('express-handlebars');//Sets our app to use the handlebars engine
 
-// const port = process.env.port || 6969
+const port = process.env.port || 6969
 
-// const app = express()
+const app = express()
+app.set('view engine', 'handlebars');//Sets handlebars configurations (we will go through them later on)
+app.engine('handlebars', handlebars());
+app.use(express.static('styles'))
 
-// const db = "mongodb://localhost/project_tracker";
-// mongoose.connect(
-//     db, 
-//     {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//         useFindAndModify: false,
-//     },
-//     err=>{
-//         if (err){
-//             console.log("You done mucked the db", err)
-//         } else {
-//             console.log("connecto to db")
-//         }
-//     }
-// )
+const db = "mongodb://localhost/project_tracker";
+mongoose.connect(
+    db, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    },
+    err=>{
+        if (err){
+            console.log("You done mucked the db", err)
+        } else {
+            console.log("connecto to db")
+        }
+    }
+)
 
 
-// app.get("/", (req, res)=>{
-//     console.log("got rooted");
-//     res.json("home");
-// });
+app.get("/", (req, res)=>{
+    console.log("got rooted");
+    res.json("home");
+});
 
-// app.use("/projects", projectRouter);
+app.use("/projects", projectRouter);
 
-// app.listen(port, ()=>console.log(`yoooo, smoke ${port} every day`));
+app.listen(port, ()=>console.log(`yoooo, smoke ${port} every day`));
